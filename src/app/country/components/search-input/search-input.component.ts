34f03ead57d6d1b3e439a74de228cb2e0bbb0f0d
jaimeaRios <jaimearios1986@gmail.com>
2026-05-19 23:02:12 -0500

    add debounce for trigger http call

--- src/app/country/components/search-input/search-input.component.ts
+++ src/app/country/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'country-search-input',
@@ -9,4 +9,19 @@ export class SearchInputComponent {
 
   placeholder = input.required<string>();
   value = output<string>();
+  debounceTime =input(300);
+
+  inputValue = signal<string>('');
+
+  debounceEffect = effect((onCleanup)=>{
+    const value = this.inputValue();
+
+    const timeout = setTimeout(()=>{
+      this.value.emit(value);
+    }, this.debounceTime());
+
+    onCleanup(()=>{
+      clearTimeout(timeout);
+    })
+  })
 }
